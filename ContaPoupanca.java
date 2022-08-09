b5f6d9b132cc76be367051047df95178e3fa7c40
Victor <victormartins991@hotmail.com>
2022-08-08 23:13:06 -0300

    project bank done

---
+++ ContaPoupanca.java
@@ -0,0 +1,15 @@
+package moduloJava.projetobanco;
+
+public class ContaPoupanca extends Conta{
+
+
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("*** Extrato da Conta Poupança *** ");
+        imprimirDadosConta();
+    }
+}
